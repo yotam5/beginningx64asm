ed15eb0e08b5c1992db3d793b2f6aaa58c7590db
YotamST <yotamm52@gmail.com>
2021-11-20 11:27:47 +0200

    m

--- redir.py
+++ redir.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import shutil
+import os
 
 
 def check_path(target_path, level=0):
@@ -12,16 +13,22 @@ def check_path(target_path, level=0):
     print_indented(target_path.name, level)
     for file in target_path.iterdir():
         name = file.name
-        if file.name[0] == '.':
+        if file.name[0] == '.' or 'bin' in file.name:
             continue
         if file.is_dir():
             check_path(file, level+1)
         else:
-            if '.out' in name or '.o' in name:
+            file_path = f'{target_path}/{file.name}'
+            a = '.o' in name
+            b = '.out' in name
+            c = os.access(f'{file_path}', os.X_OK)
+            if ((a or b) or c):
                 print_indented(file.name, level+1)
-                shutil.move(target_path, './bin')
-            else:
-                level += 1
+                try:
+                    shutil.move(f'{file_path}', './bin')
+                except Exception as e:
+                    print(e)
+            
 
 
 my_path = Path(r'.')
